b81b37dc768c20da6340ced2e2f257ae027c1020
DiyaKanwar <160303724+DiyaKanwar@users.noreply.github.com>
2025-02-01 22:35:09 +0530

    Create First_Occurrence_Index.cpp

---
+++ First_Occurrence_Index.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0) {
+            return 0;
+        }
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substr(i, m) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    string haystack1 = "sadbutsad", needle1 = "sad";
+    cout << solution.strStr(haystack1, needle1) << endl;
+    
+    string haystack2 = "leetcode", needle2 = "leeto";
+    cout << solution.strStr(haystack2, needle2) << endl;
+    
+    return 0;
+}
